5235c904c3fe3c1fdc4636ccb2f15c123b1bf56b
Dari <ili.kak.to.tak@mail.ru>
2025-09-14 12:28:15 +0300

    .

---
+++ 1 sem/ОАИП/4_lab/4_lab/var6.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <iomanip> 
+int main()
+{
+	setlocale(LC_CTYPE, "Russian");
+	using namespace std;
+	char c, probel; probel = ' ';
+	cout << "Введите символ: "; cin >> c;
+	cout << setw(11) << setfill(probel) << probel;
+	cout << setw(1) << setfill(c) << c << endl;
+	cout << setw(4) << setfill(probel) << probel;
+	cout << setw(3) << setfill(c) << c << endl;
+	cout << setw(3) << setfill(probel) << probel;
+	/*cout << setw(3) << setfill(probel) << probel;
+	cout << setw(5) << setfill(c) << c << endl;
+	cout << setw(7) << setfill(probel) << probel;
+	cout << setw(9) << setfill(c) << c << endl;
+	cout << setw(20) << setfill(probel) << probel;
+	cout << setw(16) << setfill(c) << c << endl;*/
+
+//	cout << setw(53) << setfill(probel) << probel;
+//	cout << setw(14) << setfill(c) << c << endl;
+//	cout << setw(54) << setfill(probel) << probel;
+//	cout << setw(12) << setfill(c) << c << endl;
+//	cout << setw(55) << setfill(probel) << probel;
+//	cout << setw(10) << setfill(c) << c << endl;
+//	cout << setw(55) << setfill(probel) << probel;
+}
